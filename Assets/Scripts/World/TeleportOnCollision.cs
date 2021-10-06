0f5ee38d66071610d0cf2fe5af30541d281e73b8
Gijs <gd.vis@student.avans.nl>
2021-10-06 11:47:08 +0200

    Added minimap v1
    Minimap v1 has been implemented.

--- Assets/Scripts/World/TeleportOnCollision.cs
+++ Assets/Scripts/World/TeleportOnCollision.cs
@@ -8,8 +8,6 @@ public class TeleportOnCollision : MonoBehaviour
     public GameObject player;
 
     void OnTriggerEnter() {
-        //Col.transform.position = new Vector3 (xPosition, yPosition, zPosition);
-        // Col.transform.position = new Vector3 (0, 0, 0);
         player.transform.position = teleportTarget.transform.position;
     }
 }
