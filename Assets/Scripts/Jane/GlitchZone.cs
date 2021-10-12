30599c8ba3adf8cd5280d36c3de01ec1773404e6
Gijs <gd.vis@student.avans.nl>
2021-10-12 16:54:36 +0200

    Finished v1 for glitch code
    Added script for a glitch zone.

---
+++ Assets/Scripts/Jane/GlitchZone.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlitchZone : MonoBehaviour {
+    AudioSource audioSource;
+
+    public AudioClip glitchZoneSound;
+    public GameObject[] glitchZoneComponents;
+    bool isAvailable = true;
+
+    void Start() {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    void OnTriggerEnter() {
+        audioSource.Play();
+    }
+
+    public void OnTriggerStay() {
+        if (isAvailable) {
+            
+            int componentCount = (glitchZoneComponents.Length);
+
+            int glitchObject = Random.Range(0, componentCount);
+            glitchZoneComponents[glitchObject].SetActive(false);
+
+            StartCoroutine(turnObjectOn(glitchZoneComponents[glitchObject]));
+            StartCoroutine(waitForNext());
+            isAvailable = false;
+        }
+    }
+
+    void OnTriggerExit() {
+        audioSource.Stop();
+    }
+
+    IEnumerator turnObjectOn(GameObject asset) {
+        int waitTime = Random.Range(0, 3000);
+        yield return new WaitForSeconds(waitTime / 1000);
+        asset.SetActive(true);
+    }
+
+    IEnumerator waitForNext() {
+        int waitTime = Random.Range(0, 2);
+        // yield return new WaitForSeconds(waitTime);
+        yield return new WaitForSeconds(0.2F);
+        isAvailable = true;
+    }
+
+}
