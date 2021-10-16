500144621877418566924253a7664b1857daef6e
Gijs <gd.vis@student.avans.nl>
2021-10-16 19:47:23 +0200

    Dialog v.05 done
    Dialog nearly ready. Only needs final tweaks and should is then ready.

--- Assets/Scripts/Dialog/DialogManager.cs
+++ Assets/Scripts/Dialog/DialogManager.cs
@@ -16,19 +16,22 @@ public class DialogManager : MonoBehaviour {
     public GameObject dialogParent;
     public Text title;
     public Text message;
-    public Button option1;
-    public GameObject option2;
-    public GameObject option3;
+    public GameObject option1, option2 , option3;
+    // public Button option2;
+    // public Button option3;
 
     Text option1Text;
     Text option2Text;
     Text option3Text;
 
-    Button option1Btn;
+    // Button option1Btn;
 
     Test test;
 
     bool dialogActive = false;
+    bool option1Clicked;
+    bool option2Clicked;
+    bool option3Clicked;
 
     void Start() {
         LoadDialog();
@@ -44,17 +47,47 @@ public class DialogManager : MonoBehaviour {
         option2Text = option2.GetComponentInChildren<Text>();
         option3Text = option3.GetComponentInChildren<Text>();
         
-        Button btn = option1.GetComponent<Button>();
+        // Button btn = option1.GetComponent<Button>();
         // option2 = option2.GetComponent<Button>();
         // option3 = option3.GetComponent<Button>();
+        Button option1Btn = option1.GetComponent<Button>();
+        Button option2Btn = option2.GetComponent<Button>();
+        Button option3Btn = option3.GetComponent<Button>();
 
-        btn.onClick.AddListener(testFunction);
+        // btn.onClick.AddListener(testFunction);
+        option1Btn.onClick.AddListener(delegate {catchButtonPress(0); } );
+        option2Btn.onClick.AddListener(delegate {catchButtonPress(1); } );
+        option3Btn.onClick.AddListener(delegate {catchButtonPress(2); } );
     }
 
     void Update() {
         if (Input.GetKeyDown(KeyCode.Space)) {
             changeMode();
-            executeDialog();
+            StartCoroutine(executeDialog());
+        }
+
+        // Debug.Log("Button states: ");
+        // Debug.Log(option1Clicked);
+        // Debug.Log(option2Clicked);
+        // Debug.Log(option3Clicked);
+    }
+
+    public void startDialog() {
+        changeMode();
+        StartCoroutine(executeDialog());
+    }
+
+    public void catchButtonPress(int buttonNumber) {
+        if (buttonNumber == 0) {
+            option1Clicked = true;
+        } else if (buttonNumber == 1) {
+            option2Clicked = true;
+        } else if (buttonNumber == 2) { 
+            option3Clicked = true; 
+        } else if (buttonNumber == 3) {
+            option1Clicked = false;
+            option2Clicked = false;
+            option3Clicked = false; 
         }
     }
 
@@ -67,42 +100,64 @@ public class DialogManager : MonoBehaviour {
         dialogActive = !dialogActive;
     }
 
-    public void executeDialog() {
+    public IEnumerator executeDialog() {
         dialogParent.SetActive(true);
         foreach (Dialog d in sentences) {
             if (d.type == 0) {
-                option1.enabled = false;
-                option1.enabled = false;
-                option1.enabled = false;
+                optionsState(false);
 
                 title.text = d.name;
                 message.text = d.sentence;
+                yield return waitForInput(0);
             } else if (d.type == 1) {
-                option1.enabled = true;
-                option1.enabled = true;
-                option1.enabled = true;
+                optionsState(true);
 
                 title.text = d.name;
                 if (d.sentence != null) { message.text = d.sentence; }
                 option1Text.text = d.option1;
                 option2Text.text = d.option2;
                 option3Text.text = d.option3;
+
+                yield return waitForInput(1);
+                optionsState(false);
+                if (option1Clicked) {
+                    message.text = d.response1;
+                } else if (option2Clicked) {
+                    message.text = d.response2;
+                } else if (option3Clicked) {
+                    message.text = d.response3;
+                } else {
+                    message.text = "Error";
+                }
+                yield return waitForInput(0);
+                catchButtonPress(3);
             }
         }
-        // dialogParent.SetActive(false);
+        dialogParent.SetActive(false);
+        changeMode();
     }
 
-    // private void changeDialogDisplay(int mode) {
-    //     if (mode == 0) {
-    //         option1.SetActive(false);
-    //         option2.SetActive(false);
-    //         option3.SetActice(false);
-    //     } else if (mode == 1) {
-    //         option1.SetActive(true);
-    //         option2.SetActive(true);
-    //         option3.SetActice(true);
-    //     }
-    // }
+    void optionsState(bool state) {
+        option1.SetActive(state);
+        option2.SetActive(state);
+        option3.SetActive(state);
+    }
+
+    private IEnumerator waitForInput(int condition) {
+        bool done = false;
+        while(!done) {
+            if (condition == 0) {
+                if (Input.GetKeyDown(KeyCode.F)) {
+                    done = true;
+                }
+            } else if (condition == 1) {
+                if (option1Clicked || option2Clicked || option3Clicked) {
+                    done = true;
+                }
+            }
+            yield return null;
+        }
+    }
 
     public void testFunction() {
         Debug.Log("You did a thing");
@@ -122,7 +177,7 @@ public class DialogManager : MonoBehaviour {
                 d.name = row[1];
                 d.type = sentenceType;
                 d.sentence = row[3];
-            } else if ( sentenceType == 2) {
+            } else if ( sentenceType == 1) {
                 d.name = row[1];
                 d.type = sentenceType;
                 d.option1 = row[4];
